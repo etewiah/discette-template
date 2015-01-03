1fd68f40076240b106f33854ca894b0dde19a8ae
Ed Tewiah <etewiah@hotmail.cim>
2015-01-03 22:44:01 +0100

    Basic display of topics with preloaded data

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function(params) {
+  	if (PreloadStore.data.discette_topics) {
+  		return PreloadStore.data.discette_topics;
+  	}
+  	else{
+  		debugger;
+  	}
+  },
+  setupController: function(controller, model) {
+    controller.set('model', model);
+  }
 });
