c92191e85b13a8d97b5bc50ac9217f4660b0e3d1
Ed Tewiah <etewiah@hotmail.cim>
2015-01-04 19:30:53 +0100

    Simple display of a topic

--- app/routes/topic.js
+++ app/routes/topic.js
@@ -2,12 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  // model: function(params) {
-  //   var topic = this.store.find('topic', params.id);
-  //   return topic;
-  // },
-  // setupController: function(controller, model) {
-  //   // controller.set('model', model.get('data'));
-  //   controller.set('model', model);
-  // }
+  model: function(params) {
+    // var topic = this.store.find('topic', params.id);
+    var apiUrl = "/api/topics/3";
+    var topic = $.getJSON(apiUrl).then(
+      function(detailedTopic) {
+        return detailedTopic;
+      });
+    return topic;
+  },
+  setupController: function(controller, model) {
+    // controller.set('model', model.get('data'));
+    controller.set('model', model);
+  }
 });
