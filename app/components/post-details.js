39dbbd4de78401d34ab571fb0f0396a16c7cd848
Ed Tewiah <etewiah@hotmail.cim>
2015-01-04 23:25:45 +0100

    Add avatar_url for post with some hard coded values

--- app/components/post-details.js
+++ app/components/post-details.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+
+  avatarUrl: function(){
+    var avatarUrl = "http://klavado.com" + this.post.avatar_template.replace(/\{size\}/g, '45');
+    //uploads/default/_optimized/8cf/c03/e2885952b7_45x45.JPG"; 
+    return avatarUrl;
+  }.property('value'),
 });
