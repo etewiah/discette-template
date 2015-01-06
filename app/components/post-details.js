1799fe684aada687a34665d1165d592a20adfa64
Ed Tewiah <etewiah@hotmail.cim>
2015-01-06 17:20:56 +0100

    Fix avatar attributes

--- app/components/post-details.js
+++ app/components/post-details.js
@@ -8,4 +8,9 @@ export default Ember.Component.extend({
     //uploads/default/_optimized/8cf/c03/e2885952b7_45x45.JPG"; 
     return avatarUrl;
   }.property('value'),
+  usernameUrl: function(){
+    var usernameUrl = "/users/" + this.post.username;
+    //uploads/default/_optimized/8cf/c03/e2885952b7_45x45.JPG"; 
+    return usernameUrl;
+  }.property('value'),
 });
