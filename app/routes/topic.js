8e83dc0d3fa26d6645400ff1686196038d4f935c
Ed Tewiah <etewiah@hotmail.cim>
2015-01-06 01:36:00 +0100

    Save hard-coded reply on server

--- app/routes/topic.js
+++ app/routes/topic.js
@@ -2,12 +2,38 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-    startReplyToTopic: function(){
+    startReplyToTopic: function() {
       this.controller.set('isEditing', true);
     },
-    processReplyToTopic: function(){
+    processReplyToTopic: function() {
       this.controller.set('isEditing', false);
+      var topic_id = this.controller.get('model.id');
+      var category_id = this.controller.get('model.category_id');
+      var draft = this.controller.get('model.draft');
       debugger;
+
+      var create_post_endpoint = '/posts';
+      var reply = $.ajax(create_post_endpoint, {
+        data: {
+          "api_key": "secret",
+          "api_username": "LarrisaW",
+          "raw": "Need to see how a post is added 222..",
+          "topic_id": topic_id,
+          "archetype": "regular",
+          "category": category_id
+        },
+        method: 'POST'
+
+      });
+      var that = this;
+      reply.then(function(result) {
+        console.log(that);
+        debugger;
+      },
+      function(error){
+        debugger;
+      });
+
     }
   },
   model: function(params) {
