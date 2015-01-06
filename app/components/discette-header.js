b1c8eb79915e394b92b9c175afcbbf44d591204d
Ed Tewiah <etewiah@hotmail.cim>
2015-01-07 00:10:37 +0100

    Add user-dropdown view

--- app/components/discette-header.js
+++ app/components/discette-header.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
+var originalZIndex;
+
 export default Ember.Component.extend({
   actions: {
     showSignUp: function() {
@@ -9,10 +11,96 @@ export default Ember.Component.extend({
   currentUser: function() {
     var userJson = PreloadStore.get('currentUser');
     if (userJson) {
-    	userJson.avatarUrl = "http://klavado.com" + userJson.avatar_template.replace(/\{size\}/g, '32');
-    	return userJson;
+      userJson.avatarUrl = "http://klavado.com" + userJson.avatar_template.replace(/\{size\}/g, '32');
+      return userJson;
       // return Discourse.User.create(userJson);
     }
     return null;
-  }.property()
+  }.property(),
+  didInsertElement: function() {
+
+    var self = this;
+
+    this.$('a[data-dropdown]').on('click.dropdown', function(e) {
+      self.showDropdown.apply(self, [$(e.currentTarget)]);
+      return false;
+    });
+  },
+  renderDropdowns: false,
+
+  showDropdown: function($target) {
+    var self = this;
+// debugger;
+    if(!this.get("renderDropdowns")){
+      this.set("renderDropdowns", true);
+      Em.run.next(function(){
+        self.showDropdown($target);
+      });
+      return;
+    }
+
+    var elementId = $target.data('dropdown') || $target.data('notifications'),
+        $dropdown = $("#" + elementId),
+        $li = $target.closest('li'),
+        $ul = $target.closest('ul'),
+        $html = $('html'),
+        $header = $('header'),
+        replyZIndex = parseInt($('#reply-control').css('z-index'), 10);
+
+
+    originalZIndex = originalZIndex || $('header').css('z-index');
+
+    if(replyZIndex > 0) {
+      $header.css("z-index", replyZIndex + 1);
+    }
+
+    var controller = self.get('controller');
+    if(controller && !controller.isDestroyed){
+      controller.set('visibleDropdown', elementId);
+    }
+    // we need to ensure we are rendered,
+    //  this optimises the speed of the initial render
+    var render = $target.data('render');
+    if(render){
+      if(!this.get(render)){
+        this.set(render, true);
+        Em.run.next(this, function(){
+          this.showDropdown.apply(self, [$target]);
+        });
+        return;
+      }
+    }
+
+    var hideDropdown = function() {
+      $header.css("z-index", originalZIndex);
+      $dropdown.fadeOut('fast');
+      $li.removeClass('active');
+      $html.data('hide-dropdown', null);
+      var controller = self.get('controller');
+      if(controller && !controller.isDestroyed){
+        controller.set('visibleDropdown', null);
+      }
+      return $html.off('click.d-dropdown');
+    };
+
+    // if a dropdown is active and the user clicks on it, close it
+    if($li.hasClass('active')) { return hideDropdown(); }
+    // otherwhise, mark it as active
+    $li.addClass('active');
+    // hide the other dropdowns
+    $('li', $ul).not($li).removeClass('active');
+    $('.d-dropdown').not($dropdown).fadeOut('fast');
+    // fade it fast
+    $dropdown.fadeIn('fast');
+    // autofocus any text input field
+    $dropdown.find('input[type=text]').focus().select();
+
+    $html.on('click.d-dropdown', function(e) {
+      return $(e.target).closest('.d-dropdown').length > 0 ? true : hideDropdown.apply(self);
+    });
+
+    $html.data('hide-dropdown', hideDropdown);
+
+    return false;
+  }
 });
---
+++ tests/unit/views/user-dropdown-test.js
@@ -0,0 +1,12 @@
+import {
+  moduleFor,
+  test
+} from 'ember-qunit';
+
+moduleFor('view:user-dropdown', 'UserDropdownView');
+
+// Replace this with your real tests.
+test('it exists', function() {
+  var view = this.subject();
+  ok(view);
+});
