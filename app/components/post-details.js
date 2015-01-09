2d139662cd88114400cfd68762aaa059e4c6e459
Ed Tewiah <etewiah@hotmail.cim>
2015-01-09 18:11:33 +0100

    Add moment.js formating addon

--- app/components/post-details.js
+++ app/components/post-details.js
@@ -2,7 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-
+  longCreatedAt: function(){
+    return moment(this.post.created_at).format('MMMM Do YYYY, h:mm:ss a');
+  }.property(),
   avatarUrl: function(){
     var avatarUrl = "http://klavado.com" + this.post.avatar_template.replace(/\{size\}/g, '45');
     //uploads/default/_optimized/8cf/c03/e2885952b7_45x45.JPG"; 
