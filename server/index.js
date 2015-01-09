31c9ae1c0d5f963f3e5445b9f09d1ff1f90e266b
Ed Tewiah <etewiah@hotmail.cim>
2015-01-10 00:47:12 +0100

    Allow disabling of mocks when running ember serve

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ module.exports = function(app) {
   var mocks      = globSync('./mocks/**/*.js', { cwd: __dirname }).map(require);
   var proxies    = globSync('./proxies/**/*.js', { cwd: __dirname }).map(require);
 
+  // below allows me to be able to disable mocking by running server so:
+  // DISABLE_MOCK=true ember serve --proxy http://madrid.lvh.me:3000/
+  // from: http://stackoverflow.com/questions/26686764/ember-cli-how-to-get-proxy-to-override-http-mock
+	if (process.env.DISABLE_MOCK === 'true'){
+	    return;
+	}
+
   // Log proxy requests
   var morgan  = require('morgan');
   app.use(morgan('dev'));
