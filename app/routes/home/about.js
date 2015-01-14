3bed2b49afd9299317e3ceaf663c23c901e3cf12
Ed Tewiah <etewiah@hotmail.cim>
2015-01-14 17:35:58 +0100

    Add content-composer component and use for editing about topic

--- app/routes/home/about.js
+++ app/routes/home/about.js
@@ -5,11 +5,29 @@ export default Ember.Route.extend({
     cancelReplyToTopic: function() {
       this.controller.set('isCommenting', false);
     },
+    cancelPrimaryPostEdit: function() {
+      this.controller.set('isEditingPrimaryPost', false);
+    },
+    startEditingPrimaryPost: function() {
+      var currentUser = this.controllerFor('application').get('currentUser');
+      if (currentUser) {
+        this.controller.set('isEditingPrimaryPost', true);
+
+        var postApiUrl = "/posts/" + this.controller.get('primaryPost.id')+ ".json";
+        var that = this;
+        var post = $.getJSON(postApiUrl).then(
+          function(detailedPost) {
+            that.controller.set('primaryPostWithRaw', detailedPost);
+          });
+      } else {
+        this.send('openModal', 'modal/log_in');
+      }
+    },
     startCommentOnSection: function() {
       var currentUser = this.controllerFor('application').get('currentUser');
       if (currentUser) {
         this.controller.set('isCommenting', true);
-      } else{
+      } else {
         this.send('openModal', 'modal/log_in');
       }
     },
@@ -22,7 +40,7 @@ export default Ember.Route.extend({
       var replyData = {
         "raw": draft,
         "topic_id": topic_id,
-        "archetype": "regular",
+        "archetype": "discette",
         "category": category_id
       };
 
@@ -42,6 +60,43 @@ export default Ember.Route.extend({
           // console.log(that);
           var postStream = that.controller.get('model.post_stream');
           postStream.posts.pushObject(result);
+        },
+        function(error) {
+          // TODO - handle errors
+          // debugger;
+        });
+
+    },
+    updatePrimaryPost: function() {
+      this.controller.set('isEditingPrimaryPost', false);
+      var topic_id = this.controller.get('model.id');
+      var category_id = this.controller.get('model.category_id');
+      var updatedPost = this.controller.get('primaryPostWithRaw.raw');
+
+      var replyData = {
+        "post": {
+          "raw": updatedPost,
+          "edit_reason": ""
+        },
+        "topic_id": topic_id,
+        "archetype": "discette",
+        "category": category_id
+      };
+
+      if (EmberENV.useApiKeys) {
+        replyData.apiKey = this.get('settingsService.apiKey');
+        replyData.apiUsername = this.get('settingsService.apiUsername');
+      }
+
+      var update_post_endpoint = '/posts/' + this.controller.get('primaryPost.id');
+      var reply = $.ajax(update_post_endpoint, {
+        data: replyData,
+        method: 'PUT'
+
+      });
+      var that = this;
+      reply.then(function(result) {
+          that.controller.set('primaryPost', result.post);
         },
         function(error) {
           // TODO - handle errors
