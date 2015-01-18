da779f10d54b7ccebe9f7c63656dc45b444dc16a
Ed Tewiah <etewiah@hotmail.cim>
2015-01-18 21:46:27 +0100

    Prompt for auth before claiming section

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,20 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  beforeModel: function() {
+    if (this.get('settingsService.currentSection.status') === "unclaimed") {
+      this.transitionTo('start');
+    }
+  },
   actions: {
     startNewTopic: function() {
       var currentUser = this.controllerFor('application').get('currentUser');
       if (currentUser) {
         this.send('openModal', 'modal/new_topic');
-      } else{
+      } else {
         this.send('openModal', 'modal/log_in');
       }
     }
   },
   model: function(params) {
-  	// if (PreloadStore.data.discette_topics) {
-  	// 	return PreloadStore.data.discette_topics;
-  	// }
+    // if (PreloadStore.data.discette_topics) {
+    //  return PreloadStore.data.discette_topics;
+    // }
     var apiUrl = "/drive/section/topics";
     var topics = $.getJSON(apiUrl).then(
       function(result) {
