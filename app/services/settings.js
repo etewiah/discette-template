daa8b12be3e9f0ed5c70f7fea72bfacc3edb5965
Ed Tewiah <etewiah@hotmail.cim>
2015-01-19 16:36:05 +0100

    Fix bug when user not signed in

--- app/services/settings.js
+++ app/services/settings.js
@@ -5,11 +5,11 @@ import User from '../models/user';
 export default Ember.Object.extend({
 	 currentUser: function() {
     var userJson = PreloadStore.get('currentUser');
-    // TODO: fix below
-    userJson.rootUrl = 'http://klavado.com';
      // this.get('settingsService.currentSection.rootUrl');
     // PreloadStore.get('discetteSettings.rootDomainBaseUrl') || 'http://klavado.com';
     if (userJson) {
+    // TODO: fix below
+      userJson.rootUrl = 'http://klavado.com';
       return User.create(userJson);
     }
     return null;
