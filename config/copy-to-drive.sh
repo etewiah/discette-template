c2c54d042745ec34b312d5acaa0f5c3ebd5a02fb
Ed Tewiah <etewiah@hotmail.cim>
2015-02-03 15:19:16 +0100

    Correct script for copying built files

--- config/copy-to-drive.sh
+++ config/copy-to-drive.sh
@@ -2,17 +2,17 @@
 
 FONT_FILES=../dist/fonts
 
-rm -r ../../discourse-for-plugins/plugins/discourse-drive/public/drives/default/fonts
+rm -r ../../discourse-for-plugins/plugins/discourse-drive/public/drives/reader/fonts
 for f in $FONT_FILES
 do
-  cp -r $f ../../discourse-for-plugins/plugins/discourse-drive/public/drives/default/fonts
+  cp -r $f ../../discourse-for-plugins/plugins/discourse-drive/public/drives/reader/fonts
 done
 
-rm -r ../../discourse-for-plugins/plugins/discourse-drive/public/drives/default/assets
+rm -r ../../discourse-for-plugins/plugins/discourse-drive/public/drives/reader/assets
 ASSETT_FILES=../dist/assets
 
 for f in $ASSETT_FILES
 do
-  cp -r $f ../../discourse-for-plugins/plugins/discourse-drive/public/drives/default/assets
+  cp -r $f ../../discourse-for-plugins/plugins/discourse-drive/public/drives/reader/assets
 done
 
